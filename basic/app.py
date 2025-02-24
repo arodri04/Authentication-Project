a1ad56bd06f39610bb818e04e6200deb52b2d14b
arodri04 <augustine.rodriguez55@gmail.com>
2025-02-24 10:08:01 -0700

    Added login feature

--- basic/app.py
+++ basic/app.py
@@ -38,6 +38,39 @@ def register_user():
         "email": new_user.email
     })
 
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+    print(user.email, user.password)
+    if User == None:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    if user.password != password:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    session["user_id"] = user.id
+
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
+
+@app.route("/getname")
+def get_name():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"})
+    
+    user = User.query.filter_by(id=user_id).first()
+    
+    return jsonify({
+        "name": user.email
+    })
+
 
 
 if __name__ == "__main__":
--- basic/config.py
+++ basic/config.py
@@ -5,7 +5,8 @@ import redis
 load_dotenv()
 
 class ApplicationConfig:
-    
+    SECRET_KEY = os.environ['SECRET_KEY']
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
