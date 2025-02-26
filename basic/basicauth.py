5ebf778b0f6ffdf34609fc072d8f8518cd5b9194
arodri04 <augustine.rodriguez55@gmail.com>
2025-02-26 14:58:59 -0700

    Testing out SHA256

--- basic/basicauth.py
+++ basic/basicauth.py
@@ -1,6 +1,7 @@
+import hashlib
 #Getting Username and Password
-uname = input("Enter Username: ")
-passwd = input("Enter Password: ")
+# uname = input("Enter Username: ")
+# passwd = input("Enter Password: ")
 #Dictionary of verified users
 users = {"test": ["123", "This is the Secret Message"], "test1": ["1234", "This is the Secret Message number 2"]}
 
@@ -10,4 +11,16 @@ def verify(usern, pwd):
     else:
         print("could not verify")
 
-verify(uname, passwd)
+# verify(uname, passwd)
+
+users = {"test": ["123", "This is the Secret Message"], "test1": ["1234", "This is the Secret Message number 2"]}
+
+def hash(usern, pwd):
+    hpass = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
+    print(f"Hashed: {hpass}")
+    if usern in users and hpass == hashlib.sha256(users[usern][0].encode('utf-8')).hexdigest():
+        print(users[usern][1])
+    else:
+        print("Could not verify")
+
+hash("test", "123")
